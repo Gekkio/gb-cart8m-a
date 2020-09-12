76a555ee010746c853567989f5a443c5a20e0d73
Joonas Javanainen <joonas.javanainen@gmail.com>
2020-09-12 16:37:08 +0300

    Add code formatting tools and checks

--- scripts.py
+++ scripts.py
@@ -1,3 +1,8 @@
+import os
+
+root_dir = os.path.dirname(os.path.realpath(__file__))
+
+
 def assemble():
     from gb_cart8m_a.platform import GbCartPlatform
     from gb_cart8m_a.top import Top
@@ -12,6 +17,18 @@ def clean():
     build_dir = Path("build")
     if build_dir.exists():
         shutil.rmtree(build_dir)
+
+
+def check():
+    import black
+
+    black.main(["--check", root_dir])
+
+
+def fmt():
+    import black
+
+    black.main([root_dir])
 
 
 def test():
